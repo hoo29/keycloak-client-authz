95de0c5e208e4c009573420462064cfc04df9cdd
Huw McNamara <huwmcnamara@msn.com>
2021-07-07 14:16:51 +0100

    init

---
+++ src/main/java/space/huws/apps/ClientRoleAuthenticatorException.java
@@ -0,0 +1,13 @@
+package space.huws.apps;
+
+public class ClientRoleAuthenticatorException extends RuntimeException {
+
+    public ClientRoleAuthenticatorException(String message) {
+        super(message);
+    }
+
+    public ClientRoleAuthenticatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
